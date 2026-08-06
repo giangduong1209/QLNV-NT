843e1250ee84fed28a028d8fc9a4bf1c1d57b7df
giangduong1209 <giangduong120900@gmail.com>
2026-08-06 07:25:31 +0700

    chore: change exprition time

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -13,7 +13,7 @@ export async function encrypt(payload: SessionPayload) {
   return new SignJWT(payload as unknown as Record<string, unknown>)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("7d")
+    .setExpirationTime("1d")
     .sign(encodedKey);
 }
 
